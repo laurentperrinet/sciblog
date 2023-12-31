1ef096657bb4bd2d6b573cc8253d4f9db7fd41e8
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2023-12-31 10:17:45 +0100

    updating site

--- conf.py
+++ conf.py
@@ -944,7 +944,8 @@ IMAGE_FOLDERS = {'images': 'images'}
 # Used to create favicon link like this:
 # <link rel="name" href="file" sizes="size"/>
 FAVICONS = {
-     ("icon", "favicon.ico", "16x16"),
+#     ("icon", "favicon.ico", "16x16"),
+     ("icon", "favicon.svg", "16x16"),
 #     ("icon", "/icon_128x128.png", "128x128"),
 }
 
